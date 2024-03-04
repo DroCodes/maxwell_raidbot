ef1e902a039f1651ca8c7c5323d0f6e849fb120c
Deon Daigh <deon.daigh@gmail.com>
2024-03-03 20:02:16 -0600

    fixed index.ts postgres config issue

--- src/database/models/index.ts
+++ src/database/models/index.ts
@@ -5,36 +5,31 @@ const env = process.env.NODE_ENV || 'development';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const config = require(__dirname + '/../config/config.ts')[env];
 
-// import { Sequelize } from 'sequelize';
-// import dotenv from 'dotenv';
-//
 // dotenv.config();
-//
-// const env = process.env.NODE_ENV || 'development';
 // import config from '../config/config';
 
-const sequelize = config.url
-	? new Sequelize(config.url, config)
-	: new Sequelize(config.development);
-
 // const sequelize = config.url
-// 	? new Sequelize(config.url, {
-// 		...config,
-// 		dialectOptions: {
-// 			ssl: {
-// 				require: false,
-// 				rejectUnauthorized: false,
-// 			},
-// 		},
-// 	})
-// 	: new Sequelize({
-// 		...config.development,
-// 		dialectOptions: {
-// 			ssl: {
-// 				require: false,
-// 				rejectUnauthorized: false,
-// 			},
-// 		},
-// 	});
+// 	? new Sequelize(config.url, config)
+// 	: new Sequelize(config.development);
+
+const sequelize = config.url
+	? new Sequelize(config.url, {
+		...config,
+		dialectOptions: {
+			ssl: {
+				require: false,
+				rejectUnauthorized: false,
+			},
+		},
+	})
+	: new Sequelize({
+		...config.development,
+		dialectOptions: {
+			ssl: {
+				require: false,
+				rejectUnauthorized: false,
+			},
+		},
+	});
 
 export { Sequelize, sequelize };
