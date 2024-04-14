0926fa9df24008d1eba2752b1d2c887405d9e2be
Deon Daigh <deon.daigh@gmail.com>
2024-04-13 19:35:23 -0500

    added moment js to format time properly

--- src/services/dateHelpers/dateFormater.ts
+++ src/services/dateHelpers/dateFormater.ts
@@ -1,7 +1,7 @@
 import moment from 'moment-timezone';
 
 const convertToUnixTime = (date: Date) => {
-	const dateString = date.toString();
+	const dateString = date.toISOString();
 	const easternTz = moment(dateString).tz('America/New_York');
 	const unixTime = easternTz.unix() * 1000;
 	// const unixTime = date.getTime();
